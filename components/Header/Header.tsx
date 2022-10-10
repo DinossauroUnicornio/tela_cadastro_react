400d1cbcfccf5d7d1045f1f9fb61c8d54ba396b6
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-10 12:02:15 -0300

    inserindo CSS e criando componentes com styled

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,72 @@
-import React from 'react'
+import React from 'react';
+import styles from  '../../styles/Header.module.css';
+import { css } from '@emotion/css';
+import styled from '@emotion/styled';
+import { Head } from 'next/document';
 
-export default function Header() {
+const HeaderPage = styled.header`
+  background: rgb(96, 214, 99);
+  width: 100%;
+  display: flex;
+  align-itens: center;
+  justify-content: space-between;
+  padding: 10px;
+`
+
+const InputSearch = styled.input`
+  width: 190px;
+  border-radius: 10px;
+  border: none;
+  box-shadow: 0px 0px 4px rgba(0,0,0,0.8);
+  line-height: 20px;
+  padding-left: 10px;
+`
+
+const ButtonSearch = styled.button`
+  width: 100px;
+  border-radius: 5px;
+  border: none;
+  color: red;
+  background: transparent;
+  :hover {
+    color: green;
+  }
+`
+
+type HeaderProps = {
+  menu?: Array<string>,
+  pesquisar?: string,
+}
+
+export default function Header(props: HeaderProps) {
   return (
-    <header>
+    // <header className={styles.menusite}>
+    <HeaderPage>
         <div>
             Logotipo
         </div>
         <nav>
-            Menu
+            <ul className={ css`
+            width: 90%;
+            display: inline-flex;
+            margin: 0;
+            padding: 0;
+            & > li {
+              min-width: 120px;
+              text-align: center;
+              list-style: none;
+            }` }>
+              <li>Home</li>
+              <li>Produtos</li>
+              <li>Serviços</li>
+              <li>Contato</li>
+            </ul>
         </nav>
-        <div>
-            Busca
-        </div>
-    </header>
+        <form>
+            <InputSearch placeholder='pesquisar'></InputSearch>
+            <ButtonSearch>{props.pesquisar ? props.pesquisar : "Search"}</ButtonSearch>
+        </form>
+    </HeaderPage>
+    // </header>
   )
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 export default function loginPage() {
   return (
     <>
-        <Header />
+        <Header pesquisar='Buscar'/>
         <div>
             <h1>Bem vindo ao nosso Banco Digital</h1>
         </div>
