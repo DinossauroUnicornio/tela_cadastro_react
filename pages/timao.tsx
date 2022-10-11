e6779b532164835da2572b97723d339a9720cb61
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-11 11:34:53 -0300

    setando valor no estado

--- pages/timao.tsx
+++ pages/timao.tsx
@@ -1,9 +1,16 @@
-import { NextPageContext } from 'next/dist/shared/lib/utils';
+import { getURL, NextPageContext } from 'next/dist/shared/lib/utils';
 import React, { useState } from 'react'
 
 type TimaoProps = {
   autor: string,
   frase: string,
+}
+
+const getFrase = async (url: string) => {
+  const getUrl = url;
+  const result = await fetch(getUrl);
+  const data = await result.json();
+  return data;
 }
 
 export default function Timao(props: TimaoProps) {
@@ -16,14 +23,14 @@ export default function Timao(props: TimaoProps) {
     <>
         <h1>{frase ? frase : "Seja muito bem vindo"}</h1>
         {autor && (<h2>{autor}</h2>)}
+
+        <button onClick={(_evento) => setAutor("José Fagundes")}>Mudar Autor</button>
     </>
     )
 }
 
 Timao.getInitialProps = async (ctx: NextPageContext) => {
-  const url = 'https://type.fit/api/quotes';
-  const result = await fetch(url);
-  const data = await result.json();
+  const data = await getFrase('https://type.fit/api/quotes');
   const position = Math.floor(Math.random() * data.length)
   const frase = data[position].text;
   const autor = data[position].author;
